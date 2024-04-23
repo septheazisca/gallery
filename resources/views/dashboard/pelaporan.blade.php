c9bbcb9cf5c077cfb4f0117604731c73a82fb3ca
septheazisca <septheaziscaaurora@gmail.com>
2024-04-23 21:19:30 +0700

    menampilkan report

--- resources/views/dashboard/pelaporan.blade.php
+++ resources/views/dashboard/pelaporan.blade.php
@@ -40,7 +40,7 @@
                     <th style="width: 300px;">Jenis Laporan</th>
                     <th>Pelapor</th>
                     <th>Status</th>
-                    <th data-type="date" data-format="MM/DD/YYYY" style="width: 120px;">Waktu</th>
+                    <!-- <th data-type="date" data-format="MM/DD/YYYY" style="width: 120px;">Waktu</th> -->
                     <th style="width: 120px;">Aksi</th>
                 </thead>
                 <tbody>
@@ -53,39 +53,39 @@
                     </td>
                     <td>{{ $reports->first()->foto->user->username }}</td>
                     <td>
-                      <ul class="p-1 m-0">
-                        @foreach($reports as $report)
-                        <li>
-                          <div>{{ $report->jenisLaporan->jenis_laporan }}</div>
-                        </li>
-                        @endforeach
+                      @php
+                      $jenisLaporanUnik = $reports->pluck('jenisLaporan.jenis_laporan')->unique();
+                      $totalJenisLaporan = $jenisLaporanUnik->count();
+                      @endphp
+                      <div> Terdapat total {{ $totalJenisLaporan }} jenis laporan</div> <!-- menampilkan jumlah jenis laporan -->
+                    </td>
+                    <td>
+                      @php
+                      $pelaporUnik = $reports->pluck('pelapor.username')->unique();
+                      $totalPelapor = $pelaporUnik->count();
+                      @endphp
+                      <div>{{ $totalPelapor }} user mereport</div> <!-- menampilkan jumlah orang yang ngereport -->
                       </ul>
                     </td>
                     <td>
-                      <ul class="p-1 m-0">
-                        @foreach($reports as $report)
-                        <li>
-                          <div>{{ $report->pelapor->username }}</div> <!-- Pelapor -->
-                        </li>
-                        @endforeach
-                      </ul>
-                    </td>
-                    <td>
-                      <ul class="p-1 m-0" style="list-style-type: none;">
-                        @foreach($reports as $report)
-                        <li>
-                          <div class="badge text-bg-danger">{{ $report->status }}</div>
-                        </li>
-                        @endforeach
-                      </ul>
-                    </td>
-                    <td>
-                      @foreach($reports as $report)
-                      <div>{{ $report->created_at->format('d/m/Y') }}</div> <!-- Waktu laporan -->
+                      @php
+                      // Mengambil daftar status unik
+                      $uniqueStatuses = $reports->pluck('status')->unique(); // Mendapatkan status yang unik
+                      @endphp
+                      @foreach($uniqueStatuses as $status)
+                      <div class="badge text-bg-danger">{{ $status }}</div>
+                      @break <!-- Hanya menampilkan satu -->
                       @endforeach
                     </td>
+                    <!-- <td>
+                      @foreach($reports as $report)
+                      <div>{{ $report->created_at->format('d/m/Y') }}</div>
+                      @endforeach
+                    </td> -->
                     <td>
-                      <!-- <button class="btn btn-warning-1" data-bs-toggle="modal" data-bs-target="#detailUnggahan"><i class="bi bi-eye"></i></button> -->
+                      <button class="btn btn-warning-1" data-bs-toggle="modal" data-bs-target="#reportDetailModal-{{ $foto_id }}">
+                        <i class="bi bi-eye"></i>
+                      </button>
                       <button class="btn btn-danger-1 delete-category" data-id="{{ $foto_id }}"><i class="bi bi-trash"></i></button>
                     </td>
                   </tr>
@@ -102,6 +102,44 @@
     </div>
   </section>
 </main><!-- End #main -->
+
+<!-- Modal Tambah -->
+@foreach($groupedReports as $foto_id => $reports)
+<div class="modal fade" id="reportDetailModal-{{ $foto_id }}" tabindex="-1" data-bs-backdrop="static" data-bs-keyboard="false" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+  <div class="modal-dialog modal-lg">
+    <div class="modal-content">
+      <div class="modal-header">
+        <img src="{{ asset('gllery.png') }}" alt="" style="width: 40px;">
+        <h1 class="modal-title fs-5 ms-2 fw-semibold" id="staticBackdropLabel">Gllery - Detail Report</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <table class="table table-striped">
+          <thead>
+            <tr>
+              <th scope="col" style="width: 5%;">#</th>
+              <th scope="col">Pelapor</th>
+              <th scope="col" style="width: 50%;">Jenis Laporan</th>
+              <th scope="col" style="width: 20%;">Wakru</th>
+            </tr>
+          </thead>
+          <tbody>
+            @foreach($reports as $report)
+            <tr>
+              @php $number = 1 @endphp
+              <th scope="row">{{ $number++ }}</th>
+              <td>{{ $report->pelapor->username }}</td>
+              <td>{{ $report->jenisLaporan->jenis_laporan }}</td>
+              <td>{{ $report->created_at->format('d/m/Y') }}</td>
+            </tr>
+            @endforeach
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+</div>
+@endforeach
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 <script>
