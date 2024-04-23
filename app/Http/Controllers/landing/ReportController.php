418e3cc7684f9d641e1dd2191bb6720c7c15b769
septheazisca <septheaziscaaurora@gmail.com>
2024-04-23 15:15:33 +0700

    controller report

--- app/Http/Controllers/landing/ReportController.php
+++ app/Http/Controllers/landing/ReportController.php
@@ -13,23 +13,6 @@ class ReportController extends Controller
 {
     public function reportFoto(Request $request)
     {
-        // Validasi input yang masuk
-        // $request->validate([
-        //     'jenis_laporan' => 'required|exists:jenis_laporan,jenislaporan_id', // Pastikan ini ada di DB
-        //     'foto_id' => 'required|exists:foto,foto_id', // ID foto yang akan dilaporkan
-        // ]);
-
-        // // Buat laporan baru
-        // $report = new ReportFoto();
-        // $report->foto_id = $request->input('foto_id'); // ID foto yang dilaporkan
-        // $report->jenislaporan_id = $request->input('jenis_laporan'); // ID jenis laporan
-        // $report->pelapor_id = Auth::id(); // ID pengguna yang melaporkan
-        // $report->status = 'Panding'; // tambahkan style ini <span class="badge text-bg-danger">Panding</span>
-        // $report->save();
-
-        // // Redirect atau memberikan respons setelah laporan dibuat
-        // return redirect()->back()->with('success', 'Laporan berhasil dibuat.');
-
         // Validasi input
         $request->validate([
             'jenis_laporan' => 'required|exists:jenis_laporan,jenislaporan_id', // Pastikan jenis laporan valid
@@ -45,16 +28,16 @@ class ReportController extends Controller
         $report->save();
 
         // Hitung jumlah laporan untuk foto ini
-        $totalReports = ReportFoto::where('foto_id', $request->input('foto_id'))->count();
+        // $totalReports = ReportFoto::where('foto_id', $request->input('foto_id'))->count();
 
-        // Jika jumlah laporan lebih dari atau sama dengan 2, hapus foto
-        if ($totalReports >= 2) {
-            // Hapus foto yang dilaporkan
-            $foto = Foto::find($request->input('foto_id'));
-            if ($foto) {
-                $foto->delete();
-            }
-        }
+        // // Jika jumlah laporan lebih dari atau sama dengan 2, hapus foto
+        // if ($totalReports >= 2) {
+        //     // Hapus foto yang dilaporkan
+        //     $foto = Foto::find($request->input('foto_id'));
+        //     if ($foto) {
+        //         $foto->delete();
+        //     }
+        // }
 
         // Redirect kembali dengan pesan sukses
         return redirect()->back()->with('success', 'Laporan berhasil dibuat.');
